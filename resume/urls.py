2bebbbdea945e0d1217b61484cc7191f40d1ed89
crownwangguan <crown.wg@outlook.com>
2018-05-06 23:28:45 -0400

    did some view

--- resume/urls.py
+++ resume/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'resume'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('work/', views.work, name='work'),
 ]
--- resume/views.py
+++ resume/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Job
 
 
 def index(request):
     menu = ['work', 'education', 'skills']
-    return render(request, 'resume/index.html', {'menu': menu})
+    return render(request, 'index.html', {'menu': menu})
+
+
+def work(request):
+    jobs = Job.objects.order_by('-start').all()
+    for job in jobs:
+        job.end = job.end if job.end else 'Present'
+    return render(request, 'resume/jobs.html', {'jobs': jobs})
